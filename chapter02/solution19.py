dc92200dca14c9d8199c14374833739fe2d51ccf
kurokawa5 <ymidorikawa3612@gmail.com>
2022-12-29 01:16:45 +0900

    add solution19

---
+++ chapter02/solution19.py
@@ -0,0 +1,9 @@
+# 各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．
+# 確認にはcut, uniq, sortコマンドを用いよ．
+
+import pandas as pd
+
+df = pd.read_csv('chapter02/popular-names.txt', sep='\t', header=None)
+#print(df.iloc[:,0].value_counts())
+print(df[0].value_counts())
+
