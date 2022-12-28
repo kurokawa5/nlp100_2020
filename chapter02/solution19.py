3d5773927402e83b5283850d0de537f0fea3747b
kurokawa5 <ymidorikawa3612@gmail.com>
2022-12-29 01:49:13 +0900

    add solution19

--- chapter02/solution19.py
+++ chapter02/solution19.py
@@ -4,6 +4,6 @@
 import pandas as pd
 
 df = pd.read_csv('chapter02/popular-names.txt', sep='\t', header=None)
-#print(df.iloc[:,0].value_counts())
-print(df[0].value_counts())
+print(df.iloc[:,0].value_counts())
+#print(df[0].value_counts())
 
