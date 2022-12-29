c4dbc44a297670c16d5597c1715c85f5ae34d1f5
kurokawa5 <ymidorikawa3612@gmail.com>
2022-12-29 11:55:57 +0900

    add solution21

---
+++ chapter03/solution21.py
@@ -0,0 +1,9 @@
+# 記事中でカテゴリ名を宣言している行を抽出せよ．
+
+import pandas as pd
+
+df_j = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
+uk_text = df_j.query('title=="イギリス"')['text'].values[0]
+uk_texts = uk_text.split('\n')
+ans = list(filter(lambda x: '[Category:' in x, uk_texts))
+print(ans)
