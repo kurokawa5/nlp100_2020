137982463f34c1cf1bb0a5406f9fbf799dcf5f0c
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-01 23:43:07 +0900

    add solution30

--- chapter04/solution30.py
+++ chapter04/solution30.py
@@ -1,3 +1,6 @@
+# 形態素解析結果（neko.txt.mecab）を読み込むプログラムを実装せよ．ただし，各形態素は表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）をキーとするマッピング型に格納し，1文を形態素（マッピング型）のリストとして表現せよ．
+# 第4章の残りの問題では，ここで作ったプログラムを活用せよ．
+
 def parse_mecab(block):
     res = []
     for line in block.split('\n'):
@@ -12,7 +15,6 @@ def parse_mecab(block):
             'pos1': attr[1]
         }
         res.append(lineDict)
-
 
 filename = 'chapter04/neko.txt.mecab'
 with open(filename, mode='rt', encoding='utf-8') as f:
