641d8da7adddbcb59fbf13c7909e81b490444634
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-05 23:34:10 +0900

    update solution00.py

--- chapter01/solution00.py
+++ chapter01/solution00.py
@@ -1,4 +1,9 @@
 # Obtain the string that arranges letters of the string “stressed” in reverse order (tail to head).
 
+# Sequence types (list, tuple, string, range) support slicing.
+# Therefore, you can sort in reverse order by setting text[::-1] in the slice operation.
 text = "stressed"
 print(text[::-1])
+
+# You can also use reversed() method.
+print(''.join(reversed(text)))
