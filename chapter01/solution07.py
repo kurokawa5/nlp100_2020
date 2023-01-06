1007b11304ae3b649e4c3c0bc540b9081a0d53c5
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-06 23:17:38 +0900

    update solution07.py

--- chapter01/solution07.py
+++ chapter01/solution07.py
@@ -1,6 +1,6 @@
 # Implement a function that receives arguments, x, y, and z and returns a string
-#  “{y} is {z} at {x}”, where “{x}”, “{y}”, and “{z}” denote the values of x, y, and z, 
-# respectively. In addition, confirm the return string by giving the arguments x=12, y="temperature", 
+#  “{y} is {z} at {x}”, where “{x}”, “{y}”, and “{z}” denote the values of x, y, and z,
+# respectively. In addition, confirm the return string by giving the arguments x=12, y="temperature",
 # and z=22.4.
 
 def template_based_sentence_generation(x, y, z):
