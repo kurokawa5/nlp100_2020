7514dbb40c0af7c2ff9b1c62423b4ff61013204c
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-06 15:21:06 +0900

    update solution04.py

--- chapter01/solution04.py
+++ chapter01/solution04.py
@@ -1,6 +1,8 @@
-# Split the sentence “Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can”.
-# into words, and extract the first letter from the 1st, 5th, 6th, 7th, 8th, 9th, 15th, 16th, 19th words and the first two letters from the other words.
-# Create an associative array (dictionary object or mapping object) that maps from the extracted string to the position (offset in the sentence) of the corresponding word.
+# Split the sentence “Hi He Lied Because Boron Could Not Oxidize Fluorine.
+# New Nations Might Also Sign Peace Security Clause. Arthur King Can”. into words, and
+# extract the first letter from the 1st, 5th, 6th, 7th, 8th, 9th, 15th, 16th, 19th words and
+# the first two letters from the other words. Create an associative array (dictionary object or mapping object) 
+# that maps from the extracted string to the position (offset in the sentence) of the corresponding word.
 
 # (1) コンマ(,)、ピリオド(.)を置換する
 # (2) 文を単語に分割する
