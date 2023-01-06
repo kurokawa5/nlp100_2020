e51e1e826ca65a9dbded4ff401be1b78e1119a29
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-07 01:34:17 +0900

    update solution09.py

--- chapter01/solution09.py
+++ chapter01/solution09.py
@@ -1,13 +1,18 @@
 """
-スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．
-ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文（例えば”I couldn’t believe that I could actually 
-understand what I was reading : the phenomenal power of the human mind .”）を与え，その実行結果を確認せよ．
+Write a program with the specification:
+ - Receive a word sequence separated by space
+ - For each word in the sequence:
+    - If the word is no longer than four letters, keep the word unchanged
+    - Otherwise,
+        - Keep the first and last letters unchanged
+        - Shuffle other letters in other positions (in the middle of the word)
 
+Observe the result by giving a sentence, e.g., “I couldn’t believe that
+I could actually understand what I was reading : the phenomenal power of the human mind “.
 """
-
 import random
 
-def shuffle_word(word):
+def typoglycemia(word):
     if len(word) <= 4:
         return word
     else:
@@ -17,5 +22,5 @@ def shuffle_word(word):
         return ''.join([start] + others + [end])
 
 text = 'I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind .'
-ans = [shuffle_word(word) for word in text.split()]
+ans = [typoglycemia(word) for word in text.split()]
 print(' '.join(ans))
