97691f2302f0963e88c27aa81f70df3950d67e8f
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-06 21:07:23 +0900

    update solution06.py

--- chapter01/solution06.py
+++ chapter01/solution06.py
@@ -1,14 +1,15 @@
-# “paraparaparadise”と”paragraph”に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，
-# XとYの和集合，積集合，差集合を求めよ．さらに，’se’というbi-gramがXおよびYに含まれるかどうかを調べよ．
+# Let the sets of letter bi-grams from the words “paraparaparadise” and “paragraph” $X$ and $Y$,
+# respectively. Obtain the union, intersection, difference of the two sets.
+# In addition, check whether the bigram “se” is included in the sets $X$ and $Y$
 
 def n_gram(target, n=2):
     return [target[idx:idx + n] for idx in range(len(target) - n + 1)]
 
-text1 = 'paraparaparadise'
-text2 = 'paragraph'
+text_x = 'paraparaparadise'
+text_y = 'paragraph'
 
-X = n_gram(text1)
-Y = n_gram(text2)
+X = n_gram(text_x)
+Y = n_gram(text_y)
 
 #print(f'和集合: {set(X) | set(Y)}')
 print(f'和集合: {set(X).union(set(Y))}')
