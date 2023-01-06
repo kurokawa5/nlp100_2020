ac46bedc96f8fa390e8595b9d4da07da2cdd2fed
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-06 16:20:46 +0900

    update solution05.py

--- chapter01/solution05.py
+++ chapter01/solution05.py
@@ -1,9 +1,10 @@
-# 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．
-# この関数を用い，”I am an NLPer”という文から単語bi-gram，文字bi-gramを得よ．
+# Implement a function that obtains n-grams from a given sequence object (e.g., string and list).
+# Use this function to obtain word bi-grams and letter bi-grams from the sentence “I am an NLPer”
+
 def n_gram(target, n):
     return [target[idx:idx + n] for idx in range(len(target) - n + 1)]
 
 text = 'I am an NLPer'
 for i in range(1, 4):
-    print(n_gram(text, i))#文字
+    print(n_gram(text, i))#空白を含めた文字
     print(n_gram(text.split(' '), i))#単語
