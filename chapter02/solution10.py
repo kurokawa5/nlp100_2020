4e69ee7e8418fe2d6ac018bb42df0a3100d6fc70
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-07 11:28:54 +0900

    update solution10.py solution10.sh

--- chapter02/solution10.py
+++ chapter02/solution10.py
@@ -1,4 +1,5 @@
-# 行数をカウントせよ．確認にはwcコマンドを用いよ．
+# Count the number of lines of the file. Confirm the result by using wc command.
+
 import pandas as pd
 
 df = pd.read_csv('chapter02/popular-names.txt', sep='\t', header=None)
