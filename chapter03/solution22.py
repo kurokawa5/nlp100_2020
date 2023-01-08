73e440038e83cb0c6ec6a9952280454d29ad4576
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-08 23:49:01 +0900

    update solution22.py

--- chapter03/solution22.py
+++ chapter03/solution22.py
@@ -1,10 +1,10 @@
-# 記事中でカテゴリ名を宣言している行を抽出せよ．
-
+# Extract the category names of the article.
 import pandas as pd
 
-df_j = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
-uk_text = df_j.query('title=="イギリス"')['text'].values[0]
-uk_texts = uk_text.split('\n')
-ans = list(filter(lambda x: '[Category:' in x, uk_texts))
+df = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
+text_uk = df.query('title=="イギリス"')['text'].values[0]
+splited_text_uk = text_uk.split('\n')
+ans = list(filter(lambda x: '[Category:' in x, splited_text_uk))
+
 ans = [a.replace('[[Category:', '').replace('|*', '').replace(']]', '').replace('|元','') for a in ans]
 print(ans)
