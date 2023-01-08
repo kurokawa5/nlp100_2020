bd75536ce06f2cc2e2d6c4b223a13a0841e31205
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-08 17:45:00 +0900

    update solution20.py

--- chapter03/solution20.py
+++ chapter03/solution20.py
@@ -1,9 +1,8 @@
-# Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．
-# 問題21-29では，ここで抽出した記事本文に対して実行せよ．
+# Read the JSON documents and output the body of the article about the United Kingdom.
+# Reuse the output in problems 21-29.
 
 import pandas as pd
 
-df_j = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
-uk_text = df_j.query('title=="イギリス"')['text'].values
-#uk_text = df_j.query('title=="イギリス"')['text'].values[0]
-print(uk_text)
+df = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
+text_uk = df.query('title=="イギリス"')['text'].values[0]
+print(text_uk)
