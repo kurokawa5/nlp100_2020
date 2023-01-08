f83c3cdcfb98218aec3c71b52ca50bd0467c5be4
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-08 13:25:03 +0900

    update solution18.py

--- chapter02/solution18.py
+++ chapter02/solution18.py
@@ -1,6 +1,5 @@
-# 各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．
-# 確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
-
+# Sort the lines in descending numeric order of the third column (sort lines without changing the content of each line). 
+# Confirm the result by using sort command.
 import pandas as pd
 
 df = pd.read_csv('chapter02/popular-names.txt', sep='\t', header=None)
