37cf1406676032c4ec44d0d3f80e3686f6828d1f
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-08 20:28:09 +0900

    update solution21.py

--- chapter03/solution21.py
+++ chapter03/solution21.py
@@ -1,9 +1,9 @@
-# 記事中でカテゴリ名を宣言している行を抽出せよ．
-
+# Extract lines that define the categories of the article.
 import pandas as pd
 
-df_j = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
-uk_text = df_j.query('title=="イギリス"')['text'].values[0]
-uk_texts = uk_text.split('\n')
-ans = list(filter(lambda x: '[Category:' in x, uk_texts))
+df = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
+text_uk = df.query('title=="イギリス"')['text'].values[0]
+
+splited_text_uk = text_uk.split('\n')
+ans = list(filter(lambda x: '[Category:' in x, splited_text_uk))
 print(ans)
