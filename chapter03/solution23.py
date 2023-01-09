a467f6aecf130f33486418de1f56bb760a17fdce
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-09 14:36:55 +0900

    update solution23.py

--- chapter03/solution23.py
+++ chapter03/solution23.py
@@ -1,8 +1,15 @@
-# 記事中でカテゴリ名を宣言している行を抽出せよ．
+# Extract section names in the article with their levels.
+# For example, the level of the section is 1 for the MediaWiki markup "== Section name ==".
 import re
 import pandas as pd
 
-df_j = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
-uk_text = df_j.query('title=="イギリス"')['text'].values[0]
-for section in re.findall(r'(=+)([^=]+)\1\n', uk_text):
-    print(f'{section[1].strip()}\t{len(section[0]) - 1}')
+df = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
+text_uk = df.query('title=="イギリス"')['text'].values[0]
+
+section_pattern = r'(={2,})(.+?)={2,}'
+sections = re.findall(section_pattern, text_uk)
+
+for section in sections:
+    level = len(section[0])-1
+    name = section[1].strip()
+    print(f'セクション名: {name}, レベル: {level}')
