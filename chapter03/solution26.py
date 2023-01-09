fd15f65f13f14190bba12253c4f235d12acba0ff
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-10 02:35:02 +0900

    update solution26.py

--- chapter03/solution26.py
+++ chapter03/solution26.py
@@ -1,23 +1,17 @@
-# 25の処理時に，テンプレートの値からMediaWikiの強調マークアップ（弱い強調，強調，強い強調のすべて）
-# を除去してテキストに変換せよ（参考: マークアップ早見表）．
-# https://ja.wikipedia.org/wiki/Help:%E6%97%A9%E8%A6%8B%E8%A1%A8
-
+# In addition to the process of the problem 25,
+# remove emphasis MediaWiki markups from the values. See Help:Cheatsheet.
+# https://en.wikipedia.org/wiki/Help:Cheatsheet
 import pandas as pd
 import re
 
-def remove_stress(dc):
-    r = re.compile("'+")
-    return {k: r.sub('', v) for k, v in dc.items()}
-
 df_j = pd.read_json('chapter03/jawiki-country.json.gz', lines=True, compression='infer')
-uk_text = df_j.query('title=="イギリス"')['text'].values[0]
-uk_texts = uk_text.split('\n')
+text_uk = df_j.query('title=="イギリス"')['text'].values[0]
 
-pattern = re.compile(r'\|(.+?)\s=\s*(.+)')
-ans = {}
-for line in uk_texts:
-    r = re.search(pattern, line)
-    if r:
-        ans[r[1]] = r[2]
+template_text = re.findall(r'\{\{基礎情報 (.+?^}\})', text_uk, re.MULTILINE+re.DOTALL)[0]
 
-print(remove_stress(ans))
+template_text = re.sub("'{2,5}","", template_text)
+
+template = dict(re.findall("\|(.+?) *= *(.+?)\n(?=\||})", template_text, re.MULTILINE+re.DOTALL))
+
+for t in template:
+    print(t, template[t])
