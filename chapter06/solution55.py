b6c04c7d1754a29d56bdc490466184bdd79653a5
kurokawa5 <ymidorikawa3612@gmail.com>
2023-01-14 16:08:03 +0900

    update solution55.py

---
+++ chapter06/solution55.py
@@ -0,0 +1,70 @@
+# Create the confusion matrix of the logistic regression model from the problem 52
+# for both the training data and the test data.
+import pandas as pd
+import numpy as np
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+def preprocessing(data):
+    x = []
+    y = []
+    label = {"b":0, "e":1, "t":2, "m":3}
+
+    for title, category in data:
+        title = re.sub("[0-9]+", "0", title)
+        x.append(title.lower())
+        y.append(label[category])
+
+    return x,y
+
+def score(model, X):
+    pred = model.predict([X])
+    pred_proba = model.predict_proba([X])[0, pred]
+    return pred[0], pred_proba[0]
+
+np.random.seed(123)
+df = pd.read_csv("chapter06/NewsAggregatorDataset/newsCorpora.csv", header=None, sep="\t", names=["ID", "TITLE", "URL", "PUBLISHER", "CATEGORY", "STORY", "HOSTNAME", "TIMESTAMP"])
+df = df.loc[df["PUBLISHER"].isin(["Reuters", "Huffington Post", "Businessweek", "Contactmusic.com", "Daily Mail"]), ["TITLE", "CATEGORY"]]
+
+train, valid_test = train_test_split(df, test_size=0.2, shuffle=True)
+valid, test = train_test_split(valid_test, test_size=0.5, shuffle=True)
+
+train = np.array(train)
+valid = np.array(valid)
+test = np.array(test)
+
+X_train, Y_train = preprocessing(train)
+X_valid, Y_valid = preprocessing(valid)
+X_test, Y_test = preprocessing(test)
+
+tfidfvectorizer = TfidfVectorizer(min_df=0.001)
+
+X_train = tfidfvectorizer.fit_transform(X_train).toarray()
+X_valid = tfidfvectorizer.transform(X_valid).toarray()
+X_test = tfidfvectorizer.transform(X_test).toarray()
+
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+
+train_pred = []
+test_pred = []
+
+for X in X_train:
+    train_pred.append(score(model, X)[0])
+
+for X in X_test:
+    test_pred.append(score(model, X)[0])
+
+train_matrix = confusion_matrix(Y_train, train_pred)
+sns.heatmap(train_matrix, annot=True, cmap="hot_r")
+plt.show()
+
+test_matrix = confusion_matrix(Y_test, test_pred)
+sns.heatmap(test_matrix, annot=True, cmap="hot_r")
+plt.show()
